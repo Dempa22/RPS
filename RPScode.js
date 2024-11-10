77b38d00a378ce486727c9a9ff4e660124189e94
Dempa22 <145809708+Dempa22@users.noreply.github.com>
2024-11-10 20:51:02 +0100

    The whole game

---
+++ RPScode.js
@@ -0,0 +1,64 @@
+
+
+
+function playGame() {
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() *3);
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+function getHumanChoice() {
+    let choice = prompt("Lets play Rock, Paper, Scissors. Enter 'Rock', ' Paper' or 'Scissors'").toLowerCase();
+
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    } else {
+        console.log("Invalid choice. Please enter 'Rock', 'Paper' or 'Scissors");
+        return getHumanChoice();
+    
+    }
+} 
+let humanScore = 0
+let computerScore = 0
+
+function playRound() {
+    const computerChoice = getComputerChoice()
+    const humanChoice = getHumanChoice()
+
+    if (computerChoice === "rock" && humanChoice === "paper") {
+        humanScore++;
+        return console.log("You win! Paper beats Rock!")
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        humanScore++;
+        return console.log("You win! Scissors beat Paper!")
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        humanScore++;
+        return console.log("You win! Rock beats Scissors!")
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        computerScore++;
+        return console.log("You lose! Rock beats Scissors!")
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        computerScore++;
+        return console.log("You lose! Paper beats Rock!")
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore++;
+        return console.log("You lose! Scissors beat Paper")
+    } else {
+        return console.log("It's a tie!")
+    }
+
+}
+for (let i = 0; i < 5; i++) {
+    playRound();
+}
+ 
+}
+playGame()
